360116578d7381f4f4a393ec848c5ef5e8dd3c34
Edmand46 <theedison4@gmail.com>
2022-05-15 00:38:30 +0400

    initial

---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Easy to Use',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Ragon simple and very small server
+      </>
+    ),
+  },
+  {
+    title: 'Focus on What Matters',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Focus on your game, not on your network stack
+      </>
+    ),
+  },
+  {
+    title: 'Free',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        You would like opensource, simple and free multiplayer so
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      {/*<div className="text--center">*/}
+      {/*  <Svg className={styles.featureSvg} role="img" />*/}
+      {/*</div>*/}
+      <div className="text--center padding-horiz--md">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
