2ce07ff2fdce726d07dc51a03cf35f9d0ad17e95
Edmand46 <theedison4@gmail.com>
2022-05-15 18:07:02 +0400

    update doc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,11 @@ const config = {
         defaultLocale: 'en',
         locales: ['en'],
     },
-
+    stylesheets: [
+        {
+            href: "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"
+        }
+    ],
     presets: [
         [
             'classic',
