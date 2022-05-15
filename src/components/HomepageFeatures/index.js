fb32b0d07c04706c8176dc9be6fa983f9e3345dd
Edmand46 <theedison4@gmail.com>
2022-05-15 17:55:15 +0400

    update doc

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,39 +5,76 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    icon: '&#xf135;',
     description: (
-      <>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      </>
+        <>
+          You can use built in components for prototyping your game, or build your own solution, solid and optimized for your game.
+        </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Architecture',
+    icon: '&#xf233;',
     description: (
-      <>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      </>
+        <>
+          Room based architecture with matchmaking allows, you to forget about scaling, it just works.
+        </>
     ),
   },
   {
     title: 'Free',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    icon: '&#xf06b;',
     description: (
-      <>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-      </>
+        <>
+          Ragon server is completely free network solution, with open sourced code.
+        </>
+    ),
+  },
+  {
+    title: 'Perfomance',
+    icon: '&#xf3fd;',
+    description: (
+        <>
+          Multi-threaded server with small logic core and a balancing system between threads
+        </>
+    ),
+  },
+  {
+    title: 'No CCU limitations',
+    icon: '&#xf0c0;',
+    description: (
+        <>
+          Ragon has no restriction on simultaneous connections, only technical due to the protocol that the ENet-Sharp library uses.
+        </>
+    ),
+  },
+  {
+    title: 'Unity Integration',
+    icon: '&#xf11b;',
+    description: (
+        <>
+          Ragon has Unity SDK.
+        </>
+    ),
+  },
+  {
+    title: 'Plugins',
+    icon: '&#xf12e;',
+    description: (
+        <>
+          You can add server side logic and intercept messages from the client using room plugins, as well as customize authorization.
+        </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({icon, title, description}) {
   return (
     <div className={clsx('col col--4')}>
-      {/*<div className="text--center">*/}
-      {/*  <Svg className={styles.featureSvg} role="img" />*/}
-      {/*</div>*/}
+      <div className="text--center">
+        {}
+          <i className={`las ${styles.featureIcon}`} dangerouslySetInnerHTML={{ __html: icon }}></i>
+      </div>
       <div className="text--center padding--md">
         <h2>{title}</h2>
         <p>{description}</p>
