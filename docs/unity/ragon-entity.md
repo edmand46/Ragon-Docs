bf4938828446c226858d21cc9eba80c11dd3e0ff
Edmand46 <theedison4@gmail.com>
2022-12-11 14:29:15 +0400

    update doc

--- docs/unity/ragon-entity.md
+++ docs/unity/ragon-entity.md
@@ -6,4 +6,16 @@ sidebar_position: 1
 
 Basic required component for all networked gameobjects, contains type of entity, id, and other ragon network information
 
+Entity options:
+- Authority:
+    * **None** - Non anyone can send event on this entity
+    * **All** - All can send events to this entity
+    * **Owner** - Only owner can send events to this entity
+- Discovery:
+    * **Root** - Find all Ragon Behaviours only on root object
+    * **Root With Nested** Find all Ragon Behaviours only on root object and childs 
+- Auto Destroy:
+    * **Checked** - Destroy object on destroying entity
+    * **Unchecked** - Keep object on destroying entity
+
 ![img.png](/img/ragon-entity.png)
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -3,96 +3,108 @@ import clsx from 'clsx';
 import styles from './styles.module.css';
 
 const FeatureList = [
-  {
-    title: 'Easy to Use',
-    icon: '&#xf135;',
-    description: (
-        <>
-            Simple and small, download and start create multiplayer!
-        </>
-    ),
-  },
-  {
-    title: 'Architecture',
-    icon: '&#xf233;',
-    description: (
-        <>
-          Room based architecture with matchmaking allows, you to forget about scaling, it just works.
-        </>
-    ),
-  },
-  {
-    title: 'Free',
-    icon: '&#xf06b;',
-    description: (
-        <>
-          Ragon server is completely free network solution, with open sourced code.
-        </>
-    ),
-  },
-  {
-    title: 'Perfomance',
-    icon: '&#xf3fd;',
-    description: (
-        <>
-          High-performance server on native sockets via ENet-Sharp battle tested socket library
-        </>
-    ),
-  },
-  {
-    title: 'No CCU limitations',
-    icon: '&#xf0c0;',
-    description: (
-        <>
-          Ragon has no restriction on simultaneous connections, only technical due to the protocol that the ENet-Sharp library uses.
-        </>
-    ),
-  },
-  {
-    title: 'Unity Integration',
-    icon: '&#xf11b;',
-    description: (
-        <>
-          Ragon has Unity SDK. You can use built in components for prototyping your game, or build your own solution, solid and optimized for your game.
-        </>
-    ),
-  },
-  {
-    title: 'Plugins',
-    icon: '&#xf12e;',
-    description: (
-        <>
-          You can add server side logic and intercept events from the client using room plugins, as well as customize authorization.
-        </>
-    ),
-  },
+    {
+        title: 'Easy to Use',
+        icon: '&#xf135;',
+        description: (
+            <>
+                Simple and small, download and start create multiplayer!
+            </>
+        ),
+    },
+    {
+        title: 'Architecture',
+        icon: '&#xf233;',
+        description: (
+            <>
+                Room based architecture with matchmaking allows, you to forget about scaling, it just works.
+            </>
+        ),
+    },
+    {
+        title: 'Free',
+        icon: '&#xf06b;',
+        description: (
+            <>
+                Ragon server is completely free network solution, with open sourced code.
+            </>
+        ),
+    },
+    {
+        title: 'Perfomance',
+        icon: '&#xf3fd;',
+        description: (
+            <>
+                High-performance server on native sockets via ENet-Sharp battle tested socket library
+            </>
+        ),
+    },
+    {
+        title: '.NET Power',
+        icon: '&#xf3fd;',
+        description: (
+            <>
+                Works on new .NET 6 and 7 perfomance and solid platform from Microsoft
+            </>
+        ),
+    },
+    {
+        title: 'No CCU limitations',
+        icon: '&#xf0c0;',
+        description: (
+            <>
+                Ragon has no restriction on simultaneous connections, only technical due to the protocol that the
+                ENet-Sharp library uses.
+            </>
+        ),
+    },
+    {
+        title: 'Unity Integration',
+        icon: '&#xf11b;',
+        description: (
+            <>
+                Ragon has Unity SDK. You can use built in components for prototyping your game, or build your own
+                solution, solid and optimized for your game.
+            </>
+        ),
+    },
+    {
+        title: 'Plugins',
+        icon: '&#xf12e;',
+        description: (
+            <>
+                You can add server side logic and intercept events from the client using room plugins, as well as
+                customize authorization.
+            </>
+        ),
+    },
 ];
 
-function Feature({icon, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        {}
-          <i className={`las ${styles.featureIcon}`} dangerouslySetInnerHTML={{ __html: icon }}></i>
-      </div>
-      <div className="text--center padding--md">
-        <h2>{title}</h2>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({ icon, title, description }) {
+    return (
+        <div className={clsx('col col--4')}>
+            <div className="text--center">
+                {}
+                <i className={`las ${styles.featureIcon}`} dangerouslySetInnerHTML={{ __html: icon }}></i>
+            </div>
+            <div className="text--center padding--md">
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
